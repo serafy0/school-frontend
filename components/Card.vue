756eb1d3ec00a920a2758de43945164406889b3b
mohamedelserafy <elserafy02@gmail.com>
2021-08-14 07:24:03 +0200

    better positioning for cards

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="column">
+  <div class="column is-half-tablet is-one-third-desktop">
     <div class="card">
       <header class="card-header">
         <p class="card-header-title has-text-grey">
