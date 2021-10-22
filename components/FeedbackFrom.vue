bc83b280c213a546da6ed0eceabea5658847aa0d
mohamedelserafy <elserafy02@gmail.com>
2021-10-22 02:22:25 +0200

    feedback form

---
+++ components/FeedbackFrom.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="box">
+    {{ student_id }}
+    <form @submit.prevent="giveFeedBackToStudent">
+      <b-field label="rating">
+        <b-rate v-model="feedback.rating"> </b-rate>
+      </b-field>
+
+      <b-field label="give a comment">
+        <b-input v-model="feedback.feedback_text" type="textarea" required maxlength="255">
+        </b-input>
+      </b-field>
+
+      <b-field postion="centered" class="has-text-centered"
+        ><!-- Label left empty for spacing -->
+        <b-button type="is-primary" size="is-large" outlined native-type="submit">
+          <strong> edit </strong>
+        </b-button>
+      </b-field>
+    </form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    course: Object,
+    session: Object,
+    student_id: String,
+  },
+  data() {
+    return {
+      feedback: {
+        feedback_text: '',
+        rating: 0,
+      },
+    }
+  },
+
+  methods: {
+    async giveFeedBackToStudent() {
+      try {
+        const data = await this.$axios.$post(`/feedback`, {
+          feedback_text: this.feedback.feedback_text,
+          rating: this.feedback.rating,
+          session_id: this.session.session_id,
+          student_id: this.student_id,
+        })
+        // this.children = data
+        console.log(data)
+
+        this.$buefy.toast.open(`session feedback given `)
+      } catch (err) {
+        this.$buefy.snackbar.open({ indefinite: true, message: err.response.data.message })
+        console.log(err.response.data.message)
+
+        // return
+      }
+    },
+  },
+}
+</script>
