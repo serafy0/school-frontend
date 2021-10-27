81de96298bc1748b7c3fab15b8ec93d7c9ae7bef
mohamedelserafy <elserafy02@gmail.com>
2021-10-27 08:24:05 +0200

    index page UI update

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,17 @@
   <section class="section">
     <div class="columns is-multiline">
       <card icon="github">
-        see source code on
+        see source code at
         <a target="_blank" href="https://github.com/serafy0/school-frontend"> GitHub </a>
+      </card>
+      <card v-if="!$auth.user" icon="account-tie">
+        <NuxtLink to="/signupAsTeacher"> signup as a teacher </NuxtLink>
+      </card>
+      <card v-if="!$auth.user" icon="account-edit">
+        <NuxtLink to="/signup"> signup as a student </NuxtLink>
+      </card>
+      <card v-if="!$auth.user" icon="account-supervisor-circle">
+        <NuxtLink to="/signupAsParent"> signup as a parent </NuxtLink>
       </card>
     </div>
   </section>
